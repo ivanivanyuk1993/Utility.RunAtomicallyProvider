f78274ed38b1a12a2e8e27e0333001abc8bde731
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2022-01-27 01:43:59 +0300

    Added `RunAtomicallyAsyncProvider` and `RunAtomicallyAsyncProviderTest` to use it in `RunAtomicallyForMultipleResourceIdsAsyncProvider`

---
+++ RunAtomicallyAsyncProviderNS/RunAtomicallyAsyncProvider.cs
@@ -0,0 +1,59 @@
+﻿using ShardedQueueNS;
+
+namespace RunAtomicallyAsyncProviderNS;
+
+/// <summary>
+///     In some cases we can not use <see cref="RunAtomicallyProvider"/>,
+///     because we need to pause execution until some task completes
+///
+///     We could do it in <see cref="RunAtomicallyProvider"/>,
+///     but then we would need switch by whether function async or not,
+///     and we want to avoid it, because <see cref="RunAtomicallyProvider"/> should be light-weight
+/// </summary>
+public class RunAtomicallyAsyncProvider
+{
+    private readonly ShardedQueue<Func<Task>> _asyncActionQueue;
+
+    private int _asyncActionCount;
+
+    public RunAtomicallyAsyncProvider() : this(maxConcurrentThreadCount: Environment.ProcessorCount)
+    {
+    }
+
+    public RunAtomicallyAsyncProvider(int maxConcurrentThreadCount)
+    {
+        _asyncActionQueue = new ShardedQueue<Func<Task>>(maxConcurrentThreadCount: maxConcurrentThreadCount);
+    }
+
+    public Task<T> RunAtomicallyWithResultAsync<T>(Func<Task<T>> asyncFunctionWithResult)
+    {
+        var taskCompletionSource = new TaskCompletionSource<T>();
+        // We want to return `Task` as soon as we added our function to queue, hence we do not `await` here
+#pragma warning disable CS4014
+        RunAtomicallyAsync(asyncAction: async () =>
+#pragma warning restore CS4014
+        {
+            taskCompletionSource.SetResult(result: await asyncFunctionWithResult());
+        });
+        return taskCompletionSource.Task;
+    }
+
+    public async Task RunAtomicallyAsync(Func<Task> asyncAction)
+    {
+        if (Interlocked.Increment(location: ref _asyncActionCount) != 1)
+        {
+            _asyncActionQueue.Enqueue(item: asyncAction);
+        }
+        else
+        {
+            // If we acquired first lock, asyncAction should be executed immediately and asyncAction loop started
+            await asyncAction();
+            // Note that if Interlocked.Decrement(ref _asyncActionCount) != 0
+            // => some Thread entered first if block in method
+            // => Enqueue is guaranteed to be called
+            // => Dequeue() will not deadlock while spins until it gets item
+            while (Interlocked.Decrement(location: ref _asyncActionCount) != 0)
+                await _asyncActionQueue.DequeueOrSpin()();
+        }
+    }
+}
---
+++ RunAtomicallyAsyncProviderTestNS/RunAtomicallyAsyncProviderTest.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Threading;
+using System.Threading.Tasks;
+using CountProviderNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RunAtomicallyAsyncProviderNS;
+
+namespace RunAtomicallyAsyncProviderTestNS;
+
+[TestClass]
+public class RunAtomicallyAsyncProviderTest
+{
+    private const int AccessCount = (int) 1e5;
+
+    private static readonly Func<IScheduler>[] SingleThreadSchedulerFactoryList =
+    {
+        () => new EventLoopScheduler(),
+        () => Scheduler.CurrentThread,
+        () => Scheduler.Immediate
+    };
+
+    private static readonly Func<IScheduler>[] MultipleThreadSchedulerFactoryList =
+    {
+        () => ThreadPoolScheduler.Instance
+    };
+
+    private static IEnumerable<object[]> SingleThreadSchedulerListTestData => SingleThreadSchedulerFactoryList
+        .Select(selector: singleThreadSchedulerFactory => new[] {singleThreadSchedulerFactory});
+
+    private static IEnumerable<object[]> MultipleThreadSchedulerFactoryListTestData =>
+        MultipleThreadSchedulerFactoryList
+            .Select(selector: multipleThreadSchedulerFactory => new[] {multipleThreadSchedulerFactory});
+
+    [TestMethod]
+    [DynamicData(dynamicDataSourceName: nameof(MultipleThreadSchedulerFactoryListTestData))]
+    public void AtomicIncrementUnsafeShouldNotCauseRaceWithMultipleThreadScheduler(
+        Func<IScheduler> schedulerFactory
+    )
+    {
+        RunWithSchedulerAndHandleDisposal(
+            action: scheduler =>
+            {
+                var shardedSpinningAsyncAtomicScheduler = new RunAtomicallyAsyncProvider();
+                Assert.IsFalse(
+                    condition: CausesRaceIncrementFuncWithScheduler(
+                        incrementFunc: countProvider =>
+                        {
+                            shardedSpinningAsyncAtomicScheduler.RunAtomicallyAsync(
+                                asyncAction: countProvider.IncrementUnsafeAsync);
+                        },
+                        scheduler: scheduler
+                    )
+                );
+            },
+            schedulerFactory: schedulerFactory
+        );
+    }
+
+    [TestMethod]
+    [DynamicData(dynamicDataSourceName: nameof(SingleThreadSchedulerListTestData))]
+    public void AtomicIncrementUnsafeShouldNotCauseRaceWithSingleThreadScheduler(Func<IScheduler> schedulerFactory)
+    {
+        RunWithSchedulerAndHandleDisposal(
+            action: scheduler =>
+            {
+                var shardedSpinningAsyncAtomicScheduler = new RunAtomicallyAsyncProvider();
+                Assert.IsFalse(
+                    condition: CausesRaceIncrementFuncWithScheduler(
+                        incrementFunc: countProvider =>
+                        {
+                            shardedSpinningAsyncAtomicScheduler.RunAtomicallyAsync(
+                                asyncAction: countProvider.IncrementUnsafeAsync);
+                        },
+                        scheduler: scheduler
+                    )
+                );
+            },
+            schedulerFactory: schedulerFactory
+        );
+    }
+
+    [DataTestMethod]
+    [DynamicData(dynamicDataSourceName: nameof(MultipleThreadSchedulerFactoryListTestData))]
+    public void IncrementUnsafeShouldCauseRaceWithMultipleThreadScheduler(Func<IScheduler> schedulerFactory)
+    {
+        RunWithSchedulerAndHandleDisposal(
+            action: scheduler =>
+            {
+                Assert.IsTrue(
+                    condition: CausesRaceIncrementFuncWithScheduler(
+                        incrementFunc: unsafeCountProvider => unsafeCountProvider.IncrementUnsafe(),
+                        scheduler: scheduler
+                    )
+                );
+            },
+            schedulerFactory: schedulerFactory
+        );
+    }
+
+    [DataTestMethod]
+    [DynamicData(dynamicDataSourceName: nameof(SingleThreadSchedulerListTestData))]
+    public void IncrementUnsafeShouldNotCauseRaceWithSingleThreadScheduler(Func<IScheduler> schedulerFactory)
+    {
+        RunWithSchedulerAndHandleDisposal(
+            action: scheduler =>
+            {
+                Assert.IsFalse(
+                    condition: CausesRaceIncrementFuncWithScheduler(
+                        incrementFunc: unsafeCountProvider => unsafeCountProvider.IncrementUnsafe(),
+                        scheduler: scheduler
+                    )
+                );
+            },
+            schedulerFactory: schedulerFactory
+        );
+    }
+
+    private static bool CausesRaceIncrementFuncWithScheduler(
+        Action<CountProvider> incrementFunc,
+        IScheduler scheduler
+    )
+    {
+        var countProvider = new CountProvider();
+        var countdownEvent = new CountdownEvent(initialCount: AccessCount);
+        for (var i = 0; i < AccessCount; i++)
+            scheduler.Schedule(action: () =>
+            {
+                incrementFunc(obj: countProvider);
+                countdownEvent.Signal();
+            });
+        countdownEvent.Wait();
+
+        return AccessCount != countProvider.Count;
+    }
+
+    /// <summary>
+    ///     todo make generic and DRY
+    /// </summary>
+    private void RunWithSchedulerAndHandleDisposal(Action<IScheduler> action, Func<IScheduler> schedulerFactory)
+    {
+        var scheduler = schedulerFactory();
+        action(obj: scheduler);
+        if (scheduler is IDisposable disposableScheduler) disposableScheduler.Dispose();
+    }
+
+    /// <summary>
+    ///     todo make generic and DRY
+    /// </summary>
+    private async Task RunWithSchedulerAndHandleDisposal(
+        Func<IScheduler, Task> action,
+        Func<IScheduler> schedulerFactory
+    )
+    {
+        var scheduler = schedulerFactory();
+        try
+        {
+            await action(arg: scheduler);
+        }
+        finally
+        {
+            if (scheduler is IDisposable disposableScheduler) disposableScheduler.Dispose();
+        }
+    }
+}
